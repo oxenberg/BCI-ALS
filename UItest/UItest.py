cc29e9c58dad4eb4c2c8afefb3d1b26fcb8adab9
Or Oxenberg <oxenberg14@gmail.com>
2021-03-24 14:19:10 +0200

    first iteartion of inputModule need to test

--- UItest/UItest.py
+++ UItest/UItest.py
@@ -1,5 +1,5 @@
 import time
-
+from inputModule import read_params
 
 class UItest:
     def __init__(self):
@@ -7,26 +7,42 @@ class UItest:
 
         :rtype: object
         """
+        self.params = read_params()
         self.current_time = 0
         self.time_to_wait = 2000
-    def new_game(self,action):
+        self.round = 0
+    def new_game(self, action):
         time.sleep(self.time_to_wait)
+        self.round +=1
         self.current_time = 0
         print("####new game####")
-        
+
         arrows = {
-            1 : "<--",
-            2 : "-->",
-            3 : "--"
+            1: "<--",
+            2: "-->",
+            3: "--"
         }
-        
+
         print(f"{arrows[action]}")
-    def step(self,prediction):
+
+    def step(self, prediction):
         '''
 
         :param prediction: int with predicted move
         :return:
         '''
-        self.current_time+=1
+        self.current_time += 1
+
     def get_game_time(self):
         return self.current_time
+
+    def end_game(self):
+        if self.current_time >= self.params["GAME_TIME_LIMIT"]:
+            return True
+        else:
+            return False
+
+    def end_all_rounds(self):
+        return False
+    def get_round(self):
+        return self.round
--- inputModule/inputModule.py
+++ inputModule/inputModule.py
@@ -1,7 +1,7 @@
 from pyOpenBCI import OpenBCICyton
-import json
 import time
 import numpy as np
+from collections import deque
 
 # work library
 from inputModule.utils import read_params
@@ -9,25 +9,31 @@ from UItest import UItest
 from PredictionModelModule import PredictionModel
 
 
-
-
 class SignalReader:
 
     def __init__(self):
         self.params = read_params()  # global experiment params
         self.start_time = time.time()
-        self.ITER =  0  # for counting the time
-        #windows for 1 prediction and for batch
-        self.batch = {"X" : [], "y" : []}
-        self.window_data = [] # this window have prediction data
+        self.ITER = 0  # for counting the time
+        self.int_action = self.params["DEFAULT_ACTION"]
+        self.stims = []
+        self.all_game_data = []
+        # windows for 1 prediction and for batch
+        self.batch = {"X": [], "y": []}
+        self.window_data = deque(maxlen=self.params["PREDICTION_WINDOW_SIZE"])  # this window have prediction data
 
         # modules initialization
         self.board = OpenBCICyton(port='COM3', daisy=True)
-        self.UItest = UItest() # UI module
+        self.UItest = UItest()  # UI module
+        self.prediction_model = PredictionModel()
 
-        self.board.start_expirement(self.run_expirement())
+
 
     def start_experiment(self):
+        '''
+        this function activate all the online experiment session and games.
+        '''
+
         self.board.start_stream(self.run_expirement)
         self.board.disconnect()
 
@@ -37,30 +43,40 @@ class SignalReader:
 
         all_time = time.time() - self.start_time
 
-        round_not_end = bool(self.ITER % self.params["TIME_BETWEEN_GAMES"])
+        # round_not_end = bool(self.ITER % self.params["TIME_BETWEEN_GAMES"])
+        predict_time = bool(self.UItest.get_game_time() % self.params["TIME_BETWEEN_PREDICTIONS"])
+        train_time = bool(self.UItest.get_round() % self.params["ROUNDS_BETWEEN_TRAIN"])
 
         # in the first integration the round_not_end always be 0 and the else will activate
-        if round_not_end:
-            # need to check if window full for prediction
-            if
-                PredictionModel.predict(self.window_data)
-                self.UItest.step()
+        if not self.UItest.end_game():
+            # need to check if window gap time over and if the window not empty
+            if predict_time and self.window_data:
+                prediction = PredictionModel.predict(self.window_data)
+                self.UItest.step(prediction)
+                stim = self.int_action
 
-        else:
+            else:
+                self.UItest.step()  # continue the time counting for the game
+                stim = self.params["DEFAULT_ACTION"]
+
+            self.window_data.append(data)
+            self.all_game_data.append(data)
+            self.stims.append(stim)
+
+        elif self.UItest.get_round() > 0:
+            self.prediction_model.updateModel()
             # restart window data, ##need to change if we want more
-            self.window_data = []
+            self.window_data.clear()
+            self.all_game_data = []
+            self.stims = []
+
             # start new game with random action
-            int_action = np.random.randint(1, len(self.params["ACTIONS"])+1)
-            self.UItest.new_game(int_action)
+            self.int_action = np.random.randint(1, len(self.params["ACTIONS"]) + 1)
+            self.UItest.new_game(self.int_action)
 
 
-            stim.append(0)
-
-
-        array_data.append(data)
         self.ITER["COUNT"] += 1
 
-
-        # if we finish the experiment time drop the thread
-        if int(all_time) >= self.params["EXPERIMENT_DURATION"]:
+        # if we finish the experiment stop streaming
+        if self.UItest.end_all_rounds():
             self.board.stop_stream()
